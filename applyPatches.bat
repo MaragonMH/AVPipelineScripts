206301f91293a52f6b8fa4f48dba90fa63f4958f
MartinBarufke <barufkemartin@gmail.com>
2022-11-24 19:42:05 +0100

    Verbose output

--- applyPatches.bat
+++ applyPatches.bat
@@ -2,15 +2,16 @@
 
 :: Apply patches
 for /f %%i in ('dir /b *.patch') do (set VAR=%%~ni
+echo Applying patch: %VAR%
 git -C temp checkout master
 git -C temp apply -C 1 --recount --reject --ignore-whitespace ../%VAR%.patch
 git -C temp checkout -b %VAR%
 git -C temp add -A
-git -C temp commit -m New%VAR%
+git -C temp commit -m New%VAR%)
 
 :: Zip EmbeddedContents.zip
 powershell Compress-Archive -f temp/OuterBeyond/EmbeddedContent.Content/* temp/OuterBeyond/EmbeddedContent.Content.zip
-)
+
 :: Build source
 dotnet build temp
 
